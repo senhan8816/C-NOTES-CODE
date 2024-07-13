33390cde792dfd54e48a5e9deb1a37019bce1318
luosenhan <luosenhan7@163.com>
2024-07-13 18:22:29 +0800

    'modify'

--- main.cpp
+++ main.cpp
@@ -4,14 +4,7 @@ using namespace std;
 int main()
 {
     system("chcp 65001");
+    // 接下来是p22, 无符号和有符号数字
 
-    // 数据类型
-    // 1. 短整型（short）2字节
-    // 2. 整型 （int）4字节 常用
-    // 3. 长整型 （long）windows为4字节，linux为4字节（32位），8字节（64位）
-    // 4. 长长整型 （long long）8字节
-    int num = 30;
-    // sizeof() 用于返回数据占用的字节。 下面代码输出4，因为定义的是int类型
-    cout << sizeof(num) << endl;
     return 0;
 }
