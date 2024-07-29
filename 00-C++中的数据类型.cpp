9f8766da5b3751514a9570dab682a8cc77f3ef0a
luosenhan <luosenhan7@163.com>
2024-07-29 17:42:43 +0800

    'other'

--- 00-C++中的数据类型.cpp
+++ 00-C++中的数据类型.cpp
@@ -1,9 +1,20 @@
 // 预处理指令
+// iostream 提供了cout和cin
 #include <iostream>
 using namespace std;
+// C++文件如何转换称可运行的二进制文件？
+// 1. 编译器先处理预处理语句，将对应的文件内容拷贝到代码文件中
+// 2. 开始编译，将c++代码转化为实际机器代码，每一个.cpp文件会被编译成.obj文件
+// 3. 把所有的.obj文件合并成一个.exe(windows平台)文件
+
 int main() {
     // 解决中文乱码问题
     system("chcp 65001");
+
+    // << 叫做重载运算符
+    // 将hello world 推送到cout流中，然后打印，然后再推送一个换行
+     cout << "hello world" << endl;
+     cin.get(); // 在前往下一行代码之前等待，此时代码暂停执行。直到按下回车键
 
     // C++中的数据类型：
     // 1. 基本数据类型:int，float,double,char, string,bool等
@@ -11,7 +22,8 @@ int main() {
     // 3. 指针类型：int*, char*，float* 等
     // 4. 枚举类型：enum
 
-    return 0;
+    // main函数中也可以不写return 0，不写的话，默认return 0;
+     return 0;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
-// 预处理指令
+// 预处理语句
 #include <iostream>
 using namespace std;
 
+class Person
+{
+public:
+
+    Person(int age)
+    {
+        //1、当形参和成员变量同名时，可用this指针来区分
+        this->age = age;
+    }
+
+    Person& PersonAddPerson(Person p)
+    {
+        this->age += p.age;
+        //返回对象本身
+        return *this;
+    }
+
+    int age;
+};
+
+void test01()
+{
+    Person p1(10);
+    cout << "p1.age = " << p1.age << endl;
+
+    Person p2(30);
+    p2.PersonAddPerson(p1).PersonAddPerson(p1).PersonAddPerson(p1);
+    cout << "p2.age = " << p2.age << endl;
+}
 int main() {
     // 解决中文乱码问题
     system("chcp 65001");
+    test01();
     return 0;
 }
 
